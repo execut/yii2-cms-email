084518d365dccbc8ab8ce4a56134d2f5f615fdd4
Fabio Maggio <fabio@infoweb.be>
2014-11-17 10:08:56 +0100

    Create migration file for permissions

---
+++ migrations/m141117_090705_add_default_permissions.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141117_090705_add_default_permissions extends Migration
+{
+    public function up()
+    {
+        // Create the auth items
+        $this->insert('{{%auth_item}}', [
+            'name'          => 'showEmailModule',
+            'type'          => 2,
+            'description'   => 'Show email module in main-menu',
+            'created_at'    => time(),
+            'updated_at'    => time()
+        ]);
+        
+        // Create the auth item relation
+        $this->insert('{{%auth_item_child}}', [
+            'parent'        => 'Superadmin',
+            'child'         => 'showEmailModule'
+        ]);
+    }
+
+    public function down()
+    {
+        // Delete the auth item relation       
+        $this->delete('{{%auth_item_child}}', [
+            'parent'        => 'Superadmin',
+            'child'         => 'showEmailModule'
+        ]);
+
+        // Delete the auth items
+        $this->delete('{{%auth_item}}', [
+            'name'          => 'showEmailModule',
+            'type'          => 2,
+        ]);
+    }
+}
