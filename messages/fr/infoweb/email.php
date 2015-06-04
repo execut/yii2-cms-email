f894bee9bcfcd0ab8b8b14937ee474bb0cdd82ef
Fabio Maggio <fabio@infoweb.be>
2015-06-04 10:58:11 +0200

    Update translations

---
+++ messages/fr/infoweb/email.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'Beste' => '',
+    'Email' => '',
+    'Emails' => '',
+    'Er is een nieuwe aanvraag voor een testrit met onderstaande gegevens' => '',
+    'Form' => '',
+    'From' => '',
+    'Mark as read' => '',
+    'Message' => '',
+    'Read' => '',
+    'Read at' => '',
+    'Send at' => '',
+    'Subject' => '',
+    'To' => '',
+];
--- messages/nl/infoweb/email.php
+++ messages/nl/infoweb/email.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
+    'Beste' => 'Beste',
     'Email' => 'E-mailbericht',
     'Emails' => 'E-mailberichten',
+    'Er is een nieuwe aanvraag voor een testrit met onderstaande gegevens' => '',
     'Form' => 'Formulier',
     'From' => 'Van',
     'Mark as read' => 'Markeren als gelezen',
