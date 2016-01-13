be1fdc33775be94a282a7d828004a87305d9cfb3
Fabio Maggio <fabiomaggio@gmail.com>
2016-01-13 10:23:31 +0100

    Add export functionality

--- views/email/index.php
+++ views/email/index.php
@@ -3,6 +3,7 @@
 use yii\widgets\Pjax;
 use kartik\grid\GridView;
 use kartik\helpers\Html;
+use kartik\export\ExportMenu;
 use infoweb\email\models\Email;
 use infoweb\email\assets\EmailAsset;
 
@@ -10,6 +11,49 @@ EmailAsset::register($this);
 
 $this->title = Yii::t('infoweb/email', 'Emails');
 $this->params['breadcrumbs'][] = $this->title;
+
+// Set the gridColumns
+$gridColumns = [
+    [
+        'class' => '\kartik\grid\CheckboxColumn',
+        'visible' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? true : false,
+        'checkboxOptions' => function($model) {
+            return [
+                'disabled' => (!$model->read) ? false : true
+            ];
+        }
+    ],
+    'subject',
+    [
+        'attribute' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? 'from' : 'to',
+        'label' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? Yii::t('infoweb/email', 'From') : Yii::t('infoweb/email', 'To'),
+    ],
+    'form',
+    [
+        'attribute'=>'created_at',
+        'label' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? Yii::t('infoweb/email', 'Received at') : Yii::t('infoweb/email', 'Send at'),
+        'value'=>function ($model, $index, $widget) {
+            return Yii::$app->formatter->asDate($model->created_at);
+        },
+        'filterType' => GridView::FILTER_DATE,
+        'filterWidgetOptions' => [
+            'pluginOptions' => [
+                'format' => 'dd-mm-yyyy',
+                'autoclose' => true,
+                'todayHighlight' => true,
+            ]
+        ],
+        'width' => '160px',
+        'hAlign' => 'center'
+    ],
+    [
+        'class' => 'kartik\grid\ActionColumn',
+        'template' => '{update} {delete}',
+        'updateOptions' => ['title' => Yii::t('app', 'View'), 'data-toggle' => 'tooltip'],
+        'deleteOptions'=>['title' => Yii::t('app', 'Delete'), 'data-toggle' => 'tooltip'],
+        'width' => '120px',
+    ],
+];
 ?>
 <div class="email-index">
 
@@ -21,8 +65,35 @@ $this->params['breadcrumbs'][] = $this->title;
             Yii::$app->session->get('emails.actionType'),
             Email::actionTypes()
         ); ?>
+
         <?php // Buttons ?>
-        <div class="pull-right">
+        <div class="navbar-right">
+            <?= ExportMenu::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => $gridColumns,
+                'target' => ExportMenu::TARGET_SELF,
+                'showConfirmAlert' => false,
+                'noExportColumns' => [0,5],
+                'fontAwesome' => true,
+                'columnBatchToggleSettings' => [
+                    'show' => false
+                ],
+                'exportConfig' => [
+                    ExportMenu::FORMAT_HTML => false,
+                    ExportMenu::FORMAT_CSV => [],
+                    ExportMenu::FORMAT_TEXT => false,
+                    ExportMenu::FORMAT_PDF => [],
+                    ExportMenu::FORMAT_EXCEL => false,
+                    ExportMenu::FORMAT_EXCEL_X => []
+                ],
+                'filename' => 'export',
+                'i18n' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@vendor/infoweb-internet-solutions/yii2-cms-email/messages',
+                    'forceTranslation' => true,
+                ],
+            ]); ?>
+
             <?= Html::button(Yii::t('infoweb/email', 'Mark as read'), [
                 'class' => 'btn btn-danger',
                 'id' => 'batch-read',
@@ -41,52 +112,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'id' => 'gridview',
         'dataProvider'=> $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => [
-            [
-                'class' => '\kartik\grid\CheckboxColumn',
-                'visible' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? true : false,
-                'checkboxOptions' => function($model) {
-                    return [
-                        'disabled' => (!$model->read) ? false : true
-                    ];
-                }
-            ],
-            'subject',
-            [
-                'attribute' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? 'from' : 'to',
-                'label' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? Yii::t('infoweb/email', 'From') : Yii::t('infoweb/email', 'To'),
-            ],
-            'form',
-            [
-                'attribute'=>'created_at',
-                'label' => (Yii::$app->session->get('emails.actionType') != Email::ACTION_SENT) ? Yii::t('infoweb/email', 'Received at') : Yii::t('infoweb/email', 'Send at'),
-                'value'=>function ($model, $index, $widget) {
-                    return Yii::$app->formatter->asDate($model->created_at);
-                },
-                'filterType' => GridView::FILTER_DATE,
-                'filterWidgetOptions' => [
-                    'pluginOptions' => [
-                        'format' => 'dd-mm-yyyy',
-                        'autoclose' => true,
-                        'todayHighlight' => true,
-                    ]
-                ],
-                'width' => '160px',
-                'hAlign' => 'center'
-            ],
-            [
-                'class' => 'kartik\grid\ActionColumn',
-                'template' => '{update} {delete}',
-                'updateOptions' => ['title' => Yii::t('app', 'View'), 'data-toggle' => 'tooltip'],
-                'deleteOptions'=>['title' => Yii::t('app', 'Delete'), 'data-toggle' => 'tooltip'],
-                'width' => '120px',
-            ],
-        ],
-        'responsive' => true,
-        'floatHeader' => true,
-        'floatHeaderOptions' => ['scrollingTop' => 88],
-        'hover' => true,
-        'export' => false,
+        'columns' => $gridColumns,
         'rowOptions' => function($model, $key, $index, $row) {
             return [
                 'class' => (!$model->read) ? 'unread' : 'read'
